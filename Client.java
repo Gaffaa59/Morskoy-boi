985aea269889fb5907327c92a775b5a8fcc081a6
Gaffaa59 <55506308+Gaffaa59@users.noreply.github.com>
2022-01-18 14:58:14 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -27,15 +27,19 @@ public class Client {
 
             // Создаем поток для чтения с клавиатуры.
             BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-            String line = null;
+
             /*System.out.println("Type in something and press enter. Will send it to the server and tell ya what it thinks.");
             System.out.println();*/
 
+            // Начало логики --------------------------------
+
+            String line = null; // ДЛЯ STRING
+
             while (true) {
-                /*line = keyboard.readLine(); // ждем пока пользователь введет что-то и нажмет кнопку Enter.
+                line = keyboard.readLine(); // ждем пока пользователь введет что-то и нажмет кнопку Enter.
                 System.out.println("Sending this line to the server...");
                 out.writeUTF(line); // отсылаем введенную строку текста серверу.
-                out.flush(); // заставляем поток закончить передачу данных.*/
+                out.flush(); // заставляем поток закончить передачу данных.
                 line = in.readUTF(); // ждем пока сервер отошлет строку текста.
                 System.out.println("The server was very polite. It sent me this : " + line);
                 //System.out.println("Looks like the server is pleased with us. Go ahead and enter more lines.");
